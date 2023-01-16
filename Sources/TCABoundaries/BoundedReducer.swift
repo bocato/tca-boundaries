b12b2427c565fefa31e2530a484ee01895de409e
Eduardo Bocato <dubocato@gmail.com>
2023-01-16 16:02:43 +0100

    Updating with BoundingReducers

--- Sources/TCABoundaries/BoundedReducer.swift
+++ Sources/TCABoundaries/BoundedReducer.swift
@@ -1,7 +1,7 @@
 import CasePaths
 import ComposableArchitecture
 
-public protocol BoundedReducer: ReducerProtocol where Action: TCAFeatureAction {
+public protocol BoundingReducer: ReducerProtocol where Action: TCAFeatureAction {
     func reduce(
         into state: inout State,
         viewAction action: Action.ViewAction
@@ -18,7 +18,18 @@ public protocol BoundedReducer: ReducerProtocol where Action: TCAFeatureAction {
     ) -> EffectTask<Action>
 }
 
-public extension BoundedReducer {
+public protocol ComposedBoundingReducer: BoundingReducer {
+    var body: some ReducerProtocol<State, Action> { get }
+}
+
+public extension BoundingReducer {
+    func reduce(
+        into state: inout State,
+        delegateAction action: Action.DelegateAction
+    ) -> EffectTask<Action> { .none }
+}
+
+public extension BoundingReducer where Body == Never {
     func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
         if let viewAction = (/Action.view).extract(from: action) {
             return reduce(into: &state, viewAction: viewAction)
@@ -34,12 +45,25 @@ public extension BoundedReducer {
         
         return .none
     }
-    
-    func reduce(
-        into state: inout State,
-        delegateAction action: Action.DelegateAction
-    ) -> EffectTask<Action> { .none }
 }
 
+public extension ComposedBoundingReducer {
+    func reduceCore(into state: inout State, action: Action) -> EffectTask<Action> {
+        if let action = (/Action.view).extract(from: action) {
+            return reduce(into: &state, viewAction: action)
+        }
+        if let action = (/Action._internal).extract(from: action) {
+            return reduce(into: &state, internalAction: action)
+        }
+        if let action = (/Action.delegate).extract(from: action) {
+            return reduce(into: &state, delegateAction: action)
+        }
+        return .none
+    }
+    
+    var coreReducer: Reduce<State, Action> {
+        Reduce(reduceCore)
+    }
+}
 
 
