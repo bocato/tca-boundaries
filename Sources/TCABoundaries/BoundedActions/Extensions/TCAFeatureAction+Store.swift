d0f4cd93a37c5bb76d70ecc54c936df0a9ed171c
Paul <me@pauldoes.dev>
2023-05-30 16:39:02 +0100

    Switch internal to public

--- Sources/TCABoundaries/BoundedActions/Extensions/TCAFeatureAction+Store.swift
+++ Sources/TCABoundaries/BoundedActions/Extensions/TCAFeatureAction+Store.swift
@@ -1,9 +1,9 @@
 import ComposableArchitecture
 
 /// `ViewOnlyStoreOf` For use when scoping down to a store of only `ViewAction`
-internal typealias VOStoreOf<R: BoundingReducer> = Store<R.State, R.Action.ViewAction>
+public typealias VOStoreOf<R: BoundingReducer> = Store<R.State, R.Action.ViewAction>
 /// `ViewOnlyViewStoreOf` For use when scoping down to a viewstore of only `ViewAction`
-internal typealias VOViewStoreOf<R: BoundingReducer> = ViewStore<R.State, R.Action.ViewAction>
+public typealias VOViewStoreOf<R: BoundingReducer> = ViewStore<R.State, R.Action.ViewAction>
 
 extension Store where Action: TCAFeatureAction {
     /// Convenience var to quickly scope a store to just it's `ViewAction`
